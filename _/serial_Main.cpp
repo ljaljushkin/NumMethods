9a4abf034c469a7a886230427524dd283f1c52ee
ljaljushkin <ljaljushkin.nikolaj@gmail.com>
2015-06-07 23:15:57 +0300

    Fixed crlf & lf issue

--- _/serial_Main.cpp
+++ _/serial_Main.cpp
@@ -9,113 +9,113 @@ void Outputfile(double d, char* filename);
 
 int main(int argc, char* argv[])
 {
-	int		nx,ny;
-	GetParameter(argv[1], nx, ny);
+    int		nx,ny;
+    GetParameter(argv[1], nx, ny);
 
-	double	x0 = 0, 
-			xEnd = 1,
-			x_step = (xEnd - x0) / nx, //  шаг по сетке x
-			y0 = 0,
-			yEnd = 1,
-			y_step = (yEnd - y0) / ny, // шаг по сетке y
-			**M,	// nx x ny
-			**B,	// nx x ny
-			eps = 1e-11,
-			omega = 2.0 - 10 * x_step;
+    double	x0 = 0,
+            xEnd = 1,
+            x_step = (xEnd - x0) / nx, //  пїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ x
+            y0 = 0,
+            yEnd = 1,
+            y_step = (yEnd - y0) / ny, // пїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ y
+            **M,	// nx x ny
+            **B,	// nx x ny
+            eps = 1e-11,
+            omega = 2.0 - 10 * x_step;
 
-	// инициализация матриц
-	M = new double*[nx+1];
-	for (int i=0; i < nx+1; i++) 
-	{
-		M[i] = new double[ny+1];
-		for (int j=0; j < ny+1; j++)
-		{
-			M[i][j] = sin(x0+i*x_step)*sin(y0+j*y_step);
-			if (i!=0 && i!=nx && j!=0 && j!=ny)
-			{
-				M[i][j] += -0.1 + double((double)rand() / RAND_MAX * 0.2);
-				
-			}
-		}
-	}
-	
-	B = new double*[nx+1];
-	for (int i=0; i < nx+1; i++) 
-	{
-		B[i] = new double[ny+1];
-		for (int j=0; j < ny+1; j++)
-		{
-			B[i][j] =  -2 * sin(x0+i*x_step) * sin(y0+j*y_step);
-		}
-	}
+    // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+    M = new double*[nx+1];
+    for (int i=0; i < nx+1; i++)
+    {
+        M[i] = new double[ny+1];
+        for (int j=0; j < ny+1; j++)
+        {
+            M[i][j] = sin(x0+i*x_step)*sin(y0+j*y_step);
+            if (i!=0 && i!=nx && j!=0 && j!=ny)
+            {
+                M[i][j] += -0.1 + double((double)rand() / RAND_MAX * 0.2);
 
-	double dmax=0, temp, dm=0;
-	int i,j,k=0;
-	Timer timer;
-	double x_st_2=x_step*x_step, y_st_2 = y_step*y_step;
-	
-	timer.start();
-	do {
-		k++;
-		printf("k=%i dm=%.16f eps=%.10f dm>eps=%i\n",k,dmax,eps,dmax>eps);
-		dmax = 0; // максимальное изменение значений u
-		for ( i=1; i<nx; i++ )
-			for ( j=1; j<ny; j++ ) 
-			{
-				temp = M[i][j];
-				M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] + 
-									M[i+1][j]) +
-									x_st_2*(M[i][j-1] + 
-									M[i][j+1]) - 
-									x_st_2*y_st_2* B[i][j]
-									) / (x_st_2 + y_st_2);
-				dm = fabs(temp-M[i][j]);
-				if ( dmax < dm ) dmax = dm;
-			}
-	} while ( dmax > eps && k < 10000);
-	timer.stop();
-	
-	float timeInSeconds = timer.getElapsed();
-	
-	double d_method=dmax;
-	dmax=0;
-	for ( i=0; i<nx+1; i++ )
-		for ( j=0; j<ny+1; j++ )
-		{
-			temp=sin(x0+i*x_step)*sin(y0+j*y_step);//ideal
-			dm=fabs(temp-M[i][j]);
-			if ( dmax < dm ) dmax = dm;
-		}
-	
-	char* outputfile = argv[2];
-	Outputfile(dmax , outputfile);
+            }
+        }
+    }
 
-	double duration = timer.getElapsed();
-	
-	char* timefile = argv[3];
-	double duration = timer.getElapsed();
-	Outputfile(duration, timefile);
+    B = new double*[nx+1];
+    for (int i=0; i < nx+1; i++)
+    {
+        B[i] = new double[ny+1];
+        for (int j=0; j < ny+1; j++)
+        {
+            B[i][j] =  -2 * sin(x0+i*x_step) * sin(y0+j*y_step);
+        }
+    }
 
-	delete[]M;
-	delete M;
-	delete[]B;
-	delete B;
+    double dmax=0, temp, dm=0;
+    int i,j,k=0;
+    Timer timer;
+    double x_st_2=x_step*x_step, y_st_2 = y_step*y_step;
 
-	return 0;
+    timer.start();
+    do {
+        k++;
+        printf("k=%i dm=%.16f eps=%.10f dm>eps=%i\n",k,dmax,eps,dmax>eps);
+        dmax = 0; // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ u
+        for ( i=1; i<nx; i++ )
+            for ( j=1; j<ny; j++ )
+            {
+                temp = M[i][j];
+                M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] +
+                                    M[i+1][j]) +
+                                    x_st_2*(M[i][j-1] +
+                                    M[i][j+1]) -
+                                    x_st_2*y_st_2* B[i][j]
+                                    ) / (x_st_2 + y_st_2);
+                dm = fabs(temp-M[i][j]);
+                if ( dmax < dm ) dmax = dm;
+            }
+    } while ( dmax > eps && k < 10000);
+    timer.stop();
+
+    float timeInSeconds = timer.getElapsed();
+
+    double d_method=dmax;
+    dmax=0;
+    for ( i=0; i<nx+1; i++ )
+        for ( j=0; j<ny+1; j++ )
+        {
+            temp=sin(x0+i*x_step)*sin(y0+j*y_step);//ideal
+            dm=fabs(temp-M[i][j]);
+            if ( dmax < dm ) dmax = dm;
+        }
+
+    char* outputfile = argv[2];
+    Outputfile(dmax , outputfile);
+
+    double duration = timer.getElapsed();
+
+    char* timefile = argv[3];
+    double duration = timer.getElapsed();
+    Outputfile(duration, timefile);
+
+    delete[]M;
+    delete M;
+    delete[]B;
+    delete B;
+
+    return 0;
 }
 
 void Outputfile(double d, char* filename)
 {
-	FILE* ptFile = fopen(filename, "w");
-	fprintf(ptFile, "%.30f", d);
-	fclose(ptFile);
+    FILE* ptFile = fopen(filename, "w");
+    fprintf(ptFile, "%.30f", d);
+    fclose(ptFile);
 }
 
 void GetParameter(char* filename, int& nx, int& ny)
 {
-	nx = 0;
-	ny = 0;
-	FILE* ptFile = fopen(filename, "r");
-	int m = fscanf(ptFile, "%i %i", &nx, &ny);
-	fclose(ptFile);
+    nx = 0;
+    ny = 0;
+    FILE* ptFile = fopen(filename, "r");
+    int m = fscanf(ptFile, "%i %i", &nx, &ny);
+    fclose(ptFile);
 }
--- _/tbb/WawePropagation.h
+++ _/tbb/WawePropagation.h
@@ -1,62 +1,62 @@
 class WawePropagation
 {
-	int type;//0-increase,1-stable;2-decrease
-	double **M, **B, *dm, x_step, y_step, omega;
-	int wawe, k, nx, ny;
+    int type;//0-increase,1-stable;2-decrease
+    double **M, **B, *dm, x_step, y_step, omega;
+    int wawe, k, nx, ny;
 
 public:
-	WawePropagation(double **tM, double **tB, int twawe, double *tdm, int ttype, \
-		int tk, double tx_step, double ty_step, int tnx, int tny, double tomega) : 
-					M(tM), B(tB), wawe(twawe), dm(tdm), type(ttype), k(tk), x_step(tx_step), y_step(ty_step), nx(tnx), ny(tny), omega(tomega) {}
-	void operator()( const blocked_range<int>& r ) const
-	{
-		int begin=r.begin(), end=r.end(),j;
-		double temp, d;
-		double x_st_2=x_step*x_step, y_st_2 = y_step*y_step;
-		if (type==0)
-			for ( int i=begin; i<end; i++ ) // i 1:wawe
-			{
-				j = wawe + 1 - i; // j wawe:1
-				temp = M[i][j];
-				M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] + 
-									M[i+1][j]) +
-									x_st_2*(M[i][j-1] + 
-									M[i][j+1]) - 
-									x_st_2*y_st_2* B[i][j]
-									) / (x_st_2 + y_st_2);
-				//printf("M[%i][%i]=%f\n",i,j,M[i][j]);
-				d = fabs(temp-M[i][j]);
-				if ( dm[wawe] < d ) dm[wawe] = d;
-			} 
-		else if (type == 1)
-			for ( int i=begin; i<end; i++  ) // i 1:wawe
-			{
-				j = wawe + k + 1 - i; // j wawe+k:k+1
-				temp = M[i][j];
-				M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] + 
-									M[i+1][j]) +
-									x_st_2*(M[i][j-1] + 
-									M[i][j+1]) - 
-									x_st_2*y_st_2* B[i][j]
-									) / (x_st_2 + y_st_2);
-				//printf("M[%i][%i]=%f\n",i,j,M[i][j]);
-				d = fabs(temp-M[i][j]);
-				if ( dm[wawe] < d ) dm[wawe] = d;
-			}
-		else if (type == 2)
-			for ( int i=begin; i<end; i++  ) // i nx-1-wawe:nx-2
-			{ 
-				j = ny+nx-wawe-3-i; //  j ny-2:ny-1-wawe
-				temp = M[i][j]; 
-				M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] + 
-									M[i+1][j]) +
-									x_st_2*(M[i][j-1] + 
-									M[i][j+1]) - 
-									x_st_2*y_st_2* B[i][j]
-									) / (x_st_2 + y_st_2);
-				//printf("M[%i][%i]=%f\n",i,j,M[i][j]);
-				d = fabs(temp-M[i][j]);
-				if ( dm[wawe] < d ) dm[wawe] = d;
-			} 
-	}
+    WawePropagation(double **tM, double **tB, int twawe, double *tdm, int ttype, \
+        int tk, double tx_step, double ty_step, int tnx, int tny, double tomega) :
+                    M(tM), B(tB), wawe(twawe), dm(tdm), type(ttype), k(tk), x_step(tx_step), y_step(ty_step), nx(tnx), ny(tny), omega(tomega) {}
+    void operator()( const blocked_range<int>& r ) const
+    {
+        int begin=r.begin(), end=r.end(),j;
+        double temp, d;
+        double x_st_2=x_step*x_step, y_st_2 = y_step*y_step;
+        if (type==0)
+            for ( int i=begin; i<end; i++ ) // i 1:wawe
+            {
+                j = wawe + 1 - i; // j wawe:1
+                temp = M[i][j];
+                M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] +
+                                    M[i+1][j]) +
+                                    x_st_2*(M[i][j-1] +
+                                    M[i][j+1]) -
+                                    x_st_2*y_st_2* B[i][j]
+                                    ) / (x_st_2 + y_st_2);
+                //printf("M[%i][%i]=%f\n",i,j,M[i][j]);
+                d = fabs(temp-M[i][j]);
+                if ( dm[wawe] < d ) dm[wawe] = d;
+            }
+        else if (type == 1)
+            for ( int i=begin; i<end; i++  ) // i 1:wawe
+            {
+                j = wawe + k + 1 - i; // j wawe+k:k+1
+                temp = M[i][j];
+                M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] +
+                                    M[i+1][j]) +
+                                    x_st_2*(M[i][j-1] +
+                                    M[i][j+1]) -
+                                    x_st_2*y_st_2* B[i][j]
+                                    ) / (x_st_2 + y_st_2);
+                //printf("M[%i][%i]=%f\n",i,j,M[i][j]);
+                d = fabs(temp-M[i][j]);
+                if ( dm[wawe] < d ) dm[wawe] = d;
+            }
+        else if (type == 2)
+            for ( int i=begin; i<end; i++  ) // i nx-1-wawe:nx-2
+            {
+                j = ny+nx-wawe-3-i; //  j ny-2:ny-1-wawe
+                temp = M[i][j];
+                M[i][j] =  ( 1 - omega)*M[i][j] + omega * 0.5 * (	y_st_2*(M[i-1][j] +
+                                    M[i+1][j]) +
+                                    x_st_2*(M[i][j-1] +
+                                    M[i][j+1]) -
+                                    x_st_2*y_st_2* B[i][j]
+                                    ) / (x_st_2 + y_st_2);
+                //printf("M[%i][%i]=%f\n",i,j,M[i][j]);
+                d = fabs(temp-M[i][j]);
+                if ( dm[wawe] < d ) dm[wawe] = d;
+            }
+    }
 };
